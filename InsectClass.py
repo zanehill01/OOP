07a5fb138a9709ef1d0d3088a89e14ead37a8272
zanehill01 <zane_hill1@baylor.edu>
2023-02-13 14:15:12 -0600

    small changes

--- InsectClass.py
+++ InsectClass.py
@@ -1,17 +1,19 @@
 import random
 
+
 class Insect:
 
-    def __init__(self):
-        self.wings = 2
-        self.legs = 4
+    def __init__(self, n, w, l):
+        self.name = n
+        self.wings = w
+        self.legs = l
+        self.flight = 0
+
+    def flight_length(self):
         self.flight = random.randint(0, 10)
 
-    def get_wings(self):
-        return self.wings
-    
-    def get_legs(self):
-        return self.legs
-    
-    def get_flight(self):
+    def get_miles(self):
         return self.flight
+
+    def get_name(self):
+        return self.name
--- InsectProgram.py
+++ InsectProgram.py
@@ -1,4 +1,9 @@
-import InsectClass as c
+import InsectClass as I
 
+mosquito = I.Insect('mosquito', 2, 4)
+housefly = I.Insect('housefly', 2, 4)
 
+mosquito.flight_length()
+housefly.flight_length()
 
+print(f'The {housefly.get_name()} can fly up to' {housefly.get_miles()} 'miles')
